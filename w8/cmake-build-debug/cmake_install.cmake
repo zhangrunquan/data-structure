445ddc7941719d191a3896fd7c76d8769fe94bef
zhangrunquan <34376163+zhangrunquan@users.noreply.github.com>
2018-12-09 11:13:01 +0800

    w8

--- w8/cmake-build-debug/cmake_install.cmake
+++ w8/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Clion/w8(1)
+# Install script for directory: D:/Github/data-structure/w8
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Clion/w8(1)/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Github/data-structure/w8/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
