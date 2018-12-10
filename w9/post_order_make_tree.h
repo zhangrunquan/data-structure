2a36faa2e1359792645bad5b54d6e78269248750
zrq <2041834343@qq.com>
2018-12-10 16:31:18 +0800

    w9

--- w9/post_order_make_tree.h
+++ w9/post_order_make_tree.h
@@ -8,6 +8,8 @@
 #endif //W9_POST_ORDER_MAKE_TREE_H
 
 #include <string>
+#include <algorithm>
+using  namespace std;
 /*
  * 1.为了能从根节点开始创建，倒着读字符串
  * 2.写一个类，把字符串作为构造方式
@@ -15,6 +17,11 @@
 
 class BinaryTree{
 public:
-    BinaryTree();
+    BinaryTree(string str);
 };
 
+BinaryTree::BinaryTree(string str) {
+    if(str.length()==0);
+    reverse(str.begin(),str.end());
+
+}
