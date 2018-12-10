9cfdfc4c1c86270147f36628ddcb672f51a296da
zrq <2041834343@qq.com>
2018-12-10 16:00:15 +0800

    w9

---
+++ w9/cmake-build-debug/CMakeFiles/w9.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/w9.dir/main.cpp.obj"
+  "w9.pdb"
+  "w9.exe"
+  "w9.exe.manifest"
+  "libw9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/w9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ w9/post_order_make_tree.h
@@ -0,0 +1,20 @@
+//
+// Created by zrq on 2018/12/10.
+//
+
+#ifndef W9_POST_ORDER_MAKE_TREE_H
+#define W9_POST_ORDER_MAKE_TREE_H
+
+#endif //W9_POST_ORDER_MAKE_TREE_H
+
+#include <string>
+/*
+ * 1.为了能从根节点开始创建，倒着读字符串
+ * 2.写一个类，把字符串作为构造方式
+ */
+
+class BinaryTree{
+public:
+    BinaryTree();
+};
+
